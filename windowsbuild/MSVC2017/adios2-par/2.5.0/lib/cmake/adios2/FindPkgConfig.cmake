55652b8ae3b0529944a89d9cfd43721c33bfd152
Kathleen Biagas <biagas2@llnl.gov>
2019-11-18 16:26:50 -0700

    Add adios2 libraries. (#16)

---
+++ windowsbuild/MSVC2017/adios2-par/2.5.0/lib/cmake/adios2/FindPkgConfig.cmake
@@ -0,0 +1,7 @@
+#------------------------------------------------------------------------------#
+# Distributed under the OSI-approved Apache License, Version 2.0.  See
+# accompanying file Copyright.txt for details.
+#------------------------------------------------------------------------------#
+
+# This module is already included in new versions of CMake
+include(${CMAKE_CURRENT_LIST_DIR}/upstream/FindPkgConfig.cmake)
