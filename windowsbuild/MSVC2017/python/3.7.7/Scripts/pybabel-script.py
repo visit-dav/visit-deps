ac0fcdc90553aa0b4f3f97d4a191ad78a3d49e86
Kathleen Biagas <biagas2@llnl.gov>
2020-08-06 15:34:26 -0700

    Add python 3.7.7 for windows. (#30)

---
+++ windowsbuild/MSVC2017/python/3.7.7/Scripts/pybabel-script.py
@@ -0,0 +1,12 @@
+#!C:\A_Visit\ForRegression\visit-deps\windowsbuild\MSVC2017\python\3.7.7\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'babel==2.7.0','console_scripts','pybabel'
+__requires__ = 'babel==2.7.0'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('babel==2.7.0', 'console_scripts', 'pybabel')()
+    )
---
+++ windowsbuild/MSVC2017/python/3.7.7/Scripts/rst2pseudoxml.py
@@ -0,0 +1,4 @@
+#!C:\A_Visit\ForRegression\visit-deps\windowsbuild\MSVC2017\python\3.7.7\python.exe
+# EASY-INSTALL-SCRIPT: 'docutils==0.15.2','rst2pseudoxml.py'
+__requires__ = 'docutils==0.15.2'
+__import__('pkg_resources').run_script('docutils==0.15.2', 'rst2pseudoxml.py')
