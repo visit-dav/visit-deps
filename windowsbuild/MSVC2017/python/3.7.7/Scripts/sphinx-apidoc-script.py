1f520fee27f9c998114828f5f40a05a2c68a84bb
Kathleen Biagas <biagas2@llnl.gov>
2020-08-06 15:25:15 -0700

    Add python 3.7.7 for windows.

---
+++ windowsbuild/MSVC2017/python/3.7.7/Scripts/sphinx-apidoc-script.py
@@ -0,0 +1,12 @@
+#!C:\A_Visit\ForRegression\visit-deps\windowsbuild\MSVC2017\python\3.7.7\python.exe
+# EASY-INSTALL-ENTRY-SCRIPT: 'sphinx==2.2.1','console_scripts','sphinx-apidoc'
+__requires__ = 'sphinx==2.2.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('sphinx==2.2.1', 'console_scripts', 'sphinx-apidoc')()
+    )
