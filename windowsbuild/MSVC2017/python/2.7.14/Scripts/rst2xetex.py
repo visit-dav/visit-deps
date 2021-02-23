6c5909e7421ac9c8fdc3b1488c9f0c0b3cfe34bc
biagas <biagas2@llnl.gov>
2021-02-23 09:33:49 -0800

    Remove old versions of conduit and python.

--- windowsbuild/MSVC2017/python/2.7.14/Scripts/rst2xetex.py
+++
@@ -1,4 +0,0 @@
-#!C:\A_VisIt\NewTrunk\windowsbuild\MSVC2017\python\2.7.14\python.exe
-# EASY-INSTALL-SCRIPT: 'docutils==0.14','rst2xetex.py'
-__requires__ = 'docutils==0.14'
-__import__('pkg_resources').run_script('docutils==0.14', 'rst2xetex.py')
