ebf31195c4e303fd6718a8a6a17b6556048cd049
Kathleen Biagas <biagas2@llnl.gov>
2021-03-03 17:16:15 -0800

    Add Pyside. (#66)
    Obtained from wheels.

---
+++ windowsbuild/MSVC2017/python/3.7.7/Lib/site-packages/shiboken2/_config.py
@@ -0,0 +1,11 @@
+shiboken_library_soversion = str(5.14)
+
+version = "5.14.2.3"
+version_info = (5, 14, 2.3, "", "")
+
+__build_date__ = '2020-07-06T08:09:26+00:00'
+
+
+
+
+__setup_py_package_version__ = '5.14.2.3'
