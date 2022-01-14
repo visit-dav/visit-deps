8db426bb8577a245136c3a40b4c89d66e8b2c67a
biagas <biagas2@llnl.gov>
2022-01-14 12:10:17 -0800

    Add mfem 4.3

---
+++ windowsbuild/MSVC2017/mfem/4.3/include/mfem/mesh/hexahedron.hpp
@@ -0,0 +1,75 @@
+// Copyright (c) 2010-2021, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#ifndef MFEM_HEXAHEDRON
+#define MFEM_HEXAHEDRON
+
+#include "../config/config.hpp"
+#include "element.hpp"
+
+namespace mfem
+{
+
+/// Data type hexahedron element
+class Hexahedron : public Element
+{
+protected:
+   int indices[8];
+
+public:
+   typedef Geometry::Constants<Geometry::CUBE> geom_t;
+
+   Hexahedron() : Element(Geometry::CUBE) { }
+
+   /// Constructs hexahedron by specifying the indices and the attribute.
+   Hexahedron(const int *ind, int attr = 1);
+
+   /// Constructs hexahedron by specifying the indices and the attribute.
+   Hexahedron(int ind1, int ind2, int ind3, int ind4,
+              int ind5, int ind6, int ind7, int ind8, int attr = 1);
+
+   /// Return element's type
+   Type GetType() const { return Element::HEXAHEDRON; }
+
+   /// Returns the indices of the element's vertices.
+   virtual void GetVertices(Array<int> &v) const;
+
+   virtual int *GetVertices() { return indices; }
+
+   virtual int GetNVertices() const { return 8; }
+
+   virtual int GetNEdges() const { return 12; }
+
+   virtual const int *GetEdgeVertices(int ei) const
+   { return geom_t::Edges[ei]; }
+
+   /// @deprecated Use GetNFaces(void) and GetNFaceVertices(int) instead.
+   MFEM_DEPRECATED virtual int GetNFaces(int &nFaceVertices) const
+   { nFaceVertices = 4; return 6; }
+
+   virtual int GetNFaces() const { return 6; }
+
+   virtual int GetNFaceVertices(int) const { return 4; }
+
+   virtual const int *GetFaceVertices(int fi) const
+   { return geom_t::FaceVert[fi]; }
+
+   virtual Element *Duplicate(Mesh *m) const
+   { return new Hexahedron(indices, attribute); }
+
+   virtual ~Hexahedron() { }
+};
+
+extern class TriLinear3DFiniteElement HexahedronFE;
+
+}
+
+#endif
---
+++ windowsbuild/MSVC2017/mfem/4.3/include/mfem/mesh/vertex.hpp
@@ -0,0 +1,60 @@
+// Copyright (c) 2010-2021, Lawrence Livermore National Security, LLC. Produced
+// at the Lawrence Livermore National Laboratory. All Rights reserved. See files
+// LICENSE and NOTICE for details. LLNL-CODE-806117.
+//
+// This file is part of the MFEM library. For more information and source code
+// availability visit https://mfem.org.
+//
+// MFEM is free software; you can redistribute it and/or modify it under the
+// terms of the BSD-3 license. We welcome feedback and contributions, see file
+// CONTRIBUTING.md for details.
+
+#ifndef MFEM_VERTEX
+#define MFEM_VERTEX
+
+#include "../config/config.hpp"
+#include "../general/globals.hpp"
+
+namespace mfem
+{
+
+/// Data type for vertex
+class Vertex
+{
+protected:
+   double coord[3];
+
+public:
+   Vertex() = default;
+
+   // Trivial copy constructor and trivial copy assignment operator
+
+   Vertex (double *xx, int dim);
+   Vertex( double x, double y) { coord[0] = x; coord[1] = y; coord[2] = 0.; }
+   Vertex( double x, double y, double z)
+   { coord[0] = x; coord[1] = y; coord[2] = z; }
+
+   /// Returns pointer to the coordinates of the vertex.
+   inline double * operator() () const { return (double*)coord; }
+
+   /// Returns the i'th coordinate of the vertex.
+   inline double & operator() (int i) { return coord[i]; }
+
+   /// Returns the i'th coordinate of the vertex.
+   inline const double & operator() (int i) const { return coord[i]; }
+
+   /// (DEPRECATED) Set the coordinates of the Vertex.
+   /** @deprecated This old version of SetCoords is not always memory safe. */
+   MFEM_DEPRECATED void SetCoords(const double *p)
+   { coord[0] = p[0]; coord[1] = p[1]; coord[2] = p[2]; }
+
+   /// Sets vertex location based on given point p
+   void SetCoords(int dim, const double *p)
+   { for (int i = 0; i < dim; i++) { coord[i] = p[i]; } }
+
+   // Trivial destructor
+};
+
+}
+
+#endif
