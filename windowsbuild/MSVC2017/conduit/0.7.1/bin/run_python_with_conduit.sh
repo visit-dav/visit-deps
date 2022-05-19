675557a23c7997b6c32290c13795ed2a01644dad
Kathleen Biagas <biagas2@llnl.gov>
2022-05-19 16:11:00 -0700

    Update conduit to version 0.8.3. (#109)
    * Add Conduit 0.8.3
    
    * Rebuild mfem against newer conduit.
    
    * Remove old conduit versions.

--- windowsbuild/MSVC2017/conduit/0.7.1/bin/run_python_with_conduit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-env PYTHONPATH=$PYTHONPATH:C:/A_Visit/ForRegression/visit-deps/windowsbuild/MSVC2017/conduit/0.7.1/python-modules/ C:/A_VisIt/ForRegression/visit-deps/windowsbuild/MSVC2017/python/3.7.7/python.exe $@
