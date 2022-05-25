aa99c16db2524a644886941bf5770e750995d4d3
biagas <biagas2@llnl.gov>
2022-05-25 09:04:00 -0700

    Remove old adios2 binaries.

--- windowsbuild/MSVC2017/adios2-ser/2.5.0/lib/cmake/adios2/FindPkgConfig.cmake
+++
@@ -1,7 +0,0 @@
-#------------------------------------------------------------------------------#
-# Distributed under the OSI-approved Apache License, Version 2.0.  See
-# accompanying file Copyright.txt for details.
-#------------------------------------------------------------------------------#
-
-# This module is already included in new versions of CMake
-include(${CMAKE_CURRENT_LIST_DIR}/upstream/FindPkgConfig.cmake)
