cb3b6d23397eb9568bffc1c62ab7122ebf9e413e
biagas <biagas2@llnl.gov>
2023-11-13 17:24:11 -0800

    Change python version to 3.7.16
    To resolve an issue with PIL in 3.7.7 that I couldn't resolve.
    This allows test suite to run on Windows.
    Rebuilt VTK against this new version of Python.

--- windowsbuild/MSVC2022/python/3.7.7/Scripts/rst2latex.py
+++
@@ -1,4 +0,0 @@
-#!C:\A_Visit\ForRegression\visit-deps\windowsbuild\MSVC2022\python\3.7.7\python.exe
-# EASY-INSTALL-SCRIPT: 'docutils==0.19','rst2latex.py'
-__requires__ = 'docutils==0.19'
-__import__('pkg_resources').run_script('docutils==0.19', 'rst2latex.py')
