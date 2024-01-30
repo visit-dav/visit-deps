bc9b5ee4e629feca509f48bd3c2872a30e58d2e4
biagas <biagas2@llnl.gov>
2024-01-30 12:04:43 -0800

    Add zlib 1.2.13

---
+++ windowsbuild/MSVC2022/zlib/1.2.13/cmake/zlib-targets-release.cmake
@@ -0,0 +1,19 @@
+#----------------------------------------------------------------
+# Generated CMake target import file for configuration "Release".
+#----------------------------------------------------------------
+
+# Commands may need to know the format version.
+set(CMAKE_IMPORT_FILE_VERSION 1)
+
+# Import target "zlib" for configuration "Release"
+set_property(TARGET zlib APPEND PROPERTY IMPORTED_CONFIGURATIONS RELEASE)
+set_target_properties(zlib PROPERTIES
+  IMPORTED_IMPLIB_RELEASE "${_IMPORT_PREFIX}/lib/zlib.lib"
+  IMPORTED_LOCATION_RELEASE "${_IMPORT_PREFIX}/bin/zlib.dll"
+  )
+
+list(APPEND _IMPORT_CHECK_TARGETS zlib )
+list(APPEND _IMPORT_CHECK_FILES_FOR_zlib "${_IMPORT_PREFIX}/lib/zlib.lib" "${_IMPORT_PREFIX}/bin/zlib.dll" )
+
+# Commands beyond this point should not need to know the version.
+set(CMAKE_IMPORT_FILE_VERSION)
