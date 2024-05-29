e3e9870820c15970396f990de999fc201e3c21fc
Kathleen Biagas <biagas2@llnl.gov>
2024-05-29 13:18:19 -0700

    Remove support for MSVC 2017 (#175)

--- windowsbuild/MSVC2017/conduit/0.8.7/bin/run_python_with_conduit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-env PYTHONPATH=$PYTHONPATH:C:/A_Visit/ForRegression/visit-deps/windowsbuild/MSVC2017/conduit/v0.8.7/python-modules/ C:/A_Visit/ForRegression/visit-deps/windowsbuild/MSVC2017/python/3.7.7/python.exe $@
--- windowsbuild/MSVC2017/python/3.7.7/Scripts/rst2html.py
+++
@@ -1,4 +0,0 @@
-#!C:\A_Visit\ForRegression\visit-deps\windowsbuild\MSVC2017\python\3.7.7\python.exe
-# EASY-INSTALL-SCRIPT: 'docutils==0.15.2','rst2html.py'
-__requires__ = 'docutils==0.15.2'
-__import__('pkg_resources').run_script('docutils==0.15.2', 'rst2html.py')
