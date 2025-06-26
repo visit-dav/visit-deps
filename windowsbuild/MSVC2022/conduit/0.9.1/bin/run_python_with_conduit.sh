905bd2d966eb5f0aa5e8f487526407dd2af8fd91
Kathleen Biagas <biagas2@llnl.gov>
2025-06-26 14:09:04 -0700

    Remove conduit 0.9.1.

--- windowsbuild/MSVC2022/conduit/0.9.1/bin/run_python_with_conduit.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-env PYTHONPATH=$PYTHONPATH:C:/A_VisIt/ThirdPartyBuilds/thirdparty/conduit/0.9.1/python-modules/ C:/A_VisIt/ForRegression/visit-deps/windowsbuild/MSVC2022/python/3.9.18/python.exe $@
